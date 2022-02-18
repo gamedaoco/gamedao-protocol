a4328a8b87688c5cccd1ff054668917b7482021d
Volodymyr Brazhnyk <crypto@vovacha.com>
2022-02-18 14:02:04 +0200

    rustfmt pallet-sense

--- sense/src/benchmarking.rs
+++ sense/src/benchmarking.rs
@@ -3,29 +3,28 @@ use super::*;
 
 #[allow(unused)]
 use crate::Pallet as ZeroSense;
-use frame_benchmarking::{benchmarks, impl_benchmark_test_suite, account};
+use frame_benchmarking::{account, benchmarks, impl_benchmark_test_suite};
 use frame_system::RawOrigin;
 use sp_std::vec;
 
-
-benchmarks!{
+benchmarks! {
 
     create_entity {}: _(RawOrigin::Root, account("1", 0, 0), vec![1; 256])
 
     mod_xp {
         let caller_origin = <T as frame_system::Config>::Origin::from(RawOrigin::Root);
         ZeroSense::<T>::create_entity(caller_origin, account("1", 0, 0), vec![1; 1])?;
-	}: _(RawOrigin::Root, account("1", 0, 0), 255)
+    }: _(RawOrigin::Root, account("1", 0, 0), 255)
 
     mod_rep {
         let caller_origin = <T as frame_system::Config>::Origin::from(RawOrigin::Root);
         ZeroSense::<T>::create_entity(caller_origin, account("1", 0, 0), vec![1; 1])?;
-	}: _(RawOrigin::Root, account("1", 0, 0), 255)
+    }: _(RawOrigin::Root, account("1", 0, 0), 255)
 
     mod_trust {
         let caller_origin = <T as frame_system::Config>::Origin::from(RawOrigin::Root);
         ZeroSense::<T>::create_entity(caller_origin, account("1", 0, 0), vec![1; 1])?;
-	}: _(RawOrigin::Root, account("1", 0, 0), 255)
+    }: _(RawOrigin::Root, account("1", 0, 0), 255)
 
 }
 
