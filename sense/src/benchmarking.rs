f8d2da096d43efaef1cf7625d4784edd53ce310b
Volodymyr Brazhnyk <16227101+vovacha@users.noreply.github.com>
2022-02-28 13:32:59 +0200

    Naming, formatting, small changes (#10)

--- sense/src/benchmarking.rs
+++ sense/src/benchmarking.rs
@@ -2,31 +2,30 @@
 use super::*;
 
 #[allow(unused)]
-use crate::Pallet as ZeroSense;
-use frame_benchmarking::{benchmarks, impl_benchmark_test_suite, account};
+use crate::Pallet as Sense;
+use frame_benchmarking::{account, benchmarks, impl_benchmark_test_suite};
 use frame_system::RawOrigin;
 use sp_std::vec;
 
+benchmarks! {
 
-benchmarks!{
+	create_entity {}: _(RawOrigin::Root, account("1", 0, 0), vec![1; 256])
 
-    create_entity {}: _(RawOrigin::Root, account("1", 0, 0), vec![1; 256])
-
-    mod_xp {
-        let caller_origin = <T as frame_system::Config>::Origin::from(RawOrigin::Root);
-        ZeroSense::<T>::create_entity(caller_origin, account("1", 0, 0), vec![1; 1])?;
+	mod_xp {
+		let caller_origin = <T as frame_system::Config>::Origin::from(RawOrigin::Root);
+		Sense::<T>::create_entity(caller_origin, account("1", 0, 0), vec![1; 1])?;
 	}: _(RawOrigin::Root, account("1", 0, 0), 255)
 
-    mod_rep {
-        let caller_origin = <T as frame_system::Config>::Origin::from(RawOrigin::Root);
-        ZeroSense::<T>::create_entity(caller_origin, account("1", 0, 0), vec![1; 1])?;
+	mod_rep {
+		let caller_origin = <T as frame_system::Config>::Origin::from(RawOrigin::Root);
+		Sense::<T>::create_entity(caller_origin, account("1", 0, 0), vec![1; 1])?;
 	}: _(RawOrigin::Root, account("1", 0, 0), 255)
 
-    mod_trust {
-        let caller_origin = <T as frame_system::Config>::Origin::from(RawOrigin::Root);
-        ZeroSense::<T>::create_entity(caller_origin, account("1", 0, 0), vec![1; 1])?;
+	mod_trust {
+		let caller_origin = <T as frame_system::Config>::Origin::from(RawOrigin::Root);
+		Sense::<T>::create_entity(caller_origin, account("1", 0, 0), vec![1; 1])?;
 	}: _(RawOrigin::Root, account("1", 0, 0), 255)
 
 }
 
-impl_benchmark_test_suite!(ZeroSense, crate::mock::new_test_ext(), crate::mock::Test);
+impl_benchmark_test_suite!(Sense, crate::mock::new_test_ext(), crate::mock::Test);
