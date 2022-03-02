8c1833a356a12f17e8b0c3be47d602fb415ff780
Volodymyr Brazhnyk <crypto@vovacha.com>
2022-03-02 11:17:33 +0200

    Fmt

--- flow/src/tests.rs
+++ flow/src/tests.rs
@@ -4,12 +4,12 @@ use super::*;
 use codec::{Decode, Encode};
 use frame_support::{assert_noop, assert_ok};
 use frame_system::{EventRecord, Phase};
+use gamedao_protocol_support::FlowState;
 use mock::{
-	new_test_ext, Flow, FlowProtocol, FlowGovernance, Event, Origin, Test, System, GAME, ALICE, BOB, MAX_DURATION
+    new_test_ext, Event, Flow, FlowGovernance, FlowProtocol, Origin, System, Test, ALICE, BOB,
+    GAME, MAX_DURATION,
 };
-use gamedao_protocol_support::{FlowState};
 use sp_core::H256;
-
 
 #[test]
 fn flow_create() {
@@ -23,43 +23,43 @@ fn flow_create() {
         // Error: AuthorizationError
         let not_creator = ALICE;
         assert_noop!(
-			Flow::create(
+            Flow::create(
                 Origin::signed(not_creator), H256::random(), not_creator, vec![1, 2], 0, 0, 0, 
                 FlowProtocol::Raise, FlowGovernance::No, vec![], vec![], vec![]),
-			Error::<Test>::AuthorizationError
+            Error::<Test>::AuthorizationError
 		);
         // Check if organization's treasury has enough deposit
         // Error: TreasuryBalanceTooLow
         let deposit_more_than_treasury = 1000;
         assert_noop!(
-			Flow::create(
+            Flow::create(
                 Origin::signed(BOB), H256::random(), BOB, vec![1, 2], 0, deposit_more_than_treasury, 0, 
                 FlowProtocol::Raise, FlowGovernance::No, vec![], vec![], vec![]),
-			Error::<Test>::TreasuryBalanceTooLow
+            Error::<Test>::TreasuryBalanceTooLow
 		);
         // Check if deposit is not too high
         // Error: DepositTooHigh
         let target = 10;
         let deposit_more_than_target = 20;
         assert_noop!(
-			Flow::create(
+            Flow::create(
                 Origin::signed(BOB), H256::random(), BOB, vec![1, 2], target, deposit_more_than_target, 0, 
                 FlowProtocol::Raise, FlowGovernance::No, vec![], vec![], vec![]),
-			Error::<Test>::DepositTooHigh
+            Error::<Test>::DepositTooHigh
 		);
         // Check Campaign name length
         // Error: NameTooShort
         let short_name = vec![1];
         assert_noop!(
-			Flow::create(
+            Flow::create(
                 Origin::signed(BOB), H256::random(), BOB, short_name, 20, 10, 0, 
                 FlowProtocol::Raise, FlowGovernance::No, vec![], vec![], vec![]),
-			Error::<Test>::NameTooShort
+            Error::<Test>::NameTooShort
 		);
         // Error: NameTooLong
         let long_name = vec![1, 2, 3, 4, 5];
         assert_noop!(
-			Flow::create(
+            Flow::create(
                 Origin::signed(BOB), H256::random(), BOB, long_name, 20, 10, 0, 
                 FlowProtocol::Raise, FlowGovernance::No, vec![], vec![], vec![]),
 			Error::<Test>::NameTooLong
@@ -68,19 +68,19 @@ fn flow_create() {
         // Error: EndTooEarly
         let expiration_block = current_block - 1;
         assert_noop!(
-			Flow::create(
+            Flow::create(
                 Origin::signed(BOB), H256::random(), BOB, vec![1, 2], 20, 10, expiration_block, 
                 FlowProtocol::Raise, FlowGovernance::No, vec![], vec![], vec![]),
-			Error::<Test>::EndTooEarly
+            Error::<Test>::EndTooEarly
 		);
         // Ensure campaign expires before expiration limit
         // Error: EndTooLate
         let expiration_block = MAX_DURATION + current_block + 1;
         assert_noop!(
-			Flow::create(
+            Flow::create(
                 Origin::signed(BOB), H256::random(), BOB, vec![1, 2], 20, 10, expiration_block, 
                 FlowProtocol::Raise, FlowGovernance::No, vec![], vec![], vec![]),
-			Error::<Test>::EndTooLate
+            Error::<Test>::EndTooLate
 		);
         // Check contribution limit per block
         // Error: ContributionsPerBlockExceeded
@@ -92,7 +92,7 @@ fn flow_create() {
         let deposit = 10;
         let target = 20;
         let name = vec![1, 2];
-        
+
         assert_ok!(
 			Flow::create(
                 Origin::signed(BOB), org, BOB, name.clone(), target, deposit,  expiry, 
@@ -117,20 +117,21 @@ fn flow_create() {
 
         // Events
         assert_eq!(
-			System::events(),
-			vec![
+            System::events(),
+            vec![
                 EventRecord {
-				    phase: Phase::Initialization,
-				    event: Event::Tokens(orml_tokens::Event::Reserved(2, BOB, deposit)),
-				    topics: vec![],
-			    },
+                    phase: Phase::Initialization,
+                    event: Event::Tokens(orml_tokens::Event::Reserved(2, BOB, deposit)),
+                    topics: vec![],
+                },
                 EventRecord {
-				    phase: Phase::Initialization,
-				    event: Event::Flow(crate::Event::CampaignCreated(id, BOB, BOB, target, deposit, expiry, name)),
-				    topics: vec![],
-			    },
+                    phase: Phase::Initialization,
+                    event: Event::Flow(crate::Event::CampaignCreated(
+                        id, BOB, BOB, target, deposit, expiry, name
+                    )),
+                    topics: vec![],
+                },
             ]
-		);
-
+        );
     });
 }
