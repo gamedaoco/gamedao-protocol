b8881df74b506a51f47dd6b40a1e88147cc5d5ff
vasylenko-yevhen <ggbigkon@gmail.com>
2022-03-11 22:18:26 +0200

    Use actual Sense pallet version

--- sense/src/tests.rs
+++ sense/src/tests.rs
@@ -1,5 +1,6 @@
+#![cfg(test)]
 use super::{
-    mock::*, Entity, EntityProperty, Error, Event as ZeroEvent, Sense, SenseREP, SenseTrust,
+    mock::*, Entity, EntityProperty, Error, Event as SenseEvent, SenseEntity, SenseREP, SenseTrust,
     SenseXP,
 };
 use frame_support::{assert_noop, assert_ok};
@@ -8,8 +9,8 @@ use sp_runtime::traits::BadOrigin;
 
 #[test]
 fn sense_create_entity() {
-    new_test_ext().execute_with(|| {
-        let cid = vec![1, 2, 3];
+	new_test_ext().execute_with(|| {
+		let cid = vec![1, 2, 3];
 
         let account = 1;
         let index = 0;
@@ -17,60 +18,47 @@ fn sense_create_entity() {
 
         System::set_block_number(block_number);
 
-        assert_noop!(
-            ZeroSense::create_entity(RawOrigin::Root.into(), 1, vec![]),
-            Error::<Test>::InvalidParam
-        );
-        assert_noop!(
-            ZeroSense::create_entity(RawOrigin::Root.into(), 1, vec![1u8; 257]),
-            Error::<Test>::ParamLimitExceed
-        );
+		assert_noop!(
+			Sense::create_entity(RawOrigin::Root.into(), 1, vec![]),
+			Error::<Test>::InvalidParam
+		);
+		assert_noop!(
+			Sense::create_entity(RawOrigin::Root.into(), 1, vec![1u8; 257]),
+			Error::<Test>::ParamLimitExceed
+		);
 
-        assert_ok!(ZeroSense::create_entity(
-            RawOrigin::Root.into(),
-            account,
-            cid.clone()
-        ));
+		assert_ok!(Sense::create_entity(RawOrigin::Root.into(), account, cid.clone()));
 
-        assert_eq!(
-            Entity::new(account, block_number, index, cid.clone()),
-            ZeroSense::entity(account)
-        );
-        assert_eq!(EntityProperty::new(0, block_number), ZeroSense::xp(account));
-        assert_eq!(
-            EntityProperty::new(0, block_number),
-            ZeroSense::rep(account)
-        );
-        assert_eq!(
-            EntityProperty::new(0, block_number),
-            ZeroSense::trust(account)
-        );
+		assert_eq!(
+			Entity::new(account, block_number, index, cid.clone()),
+			Sense::entity(account)
+		);
+		assert_eq!(EntityProperty::new(0, block_number), Sense::xp(account));
+		assert_eq!(EntityProperty::new(0, block_number), Sense::rep(account));
+		assert_eq!(EntityProperty::new(0, block_number), Sense::trust(account));
 
-        assert_eq!(
-            System::events(),
-            vec![EventRecord {
-                phase: Phase::Initialization,
-                event: Event::ZeroSense(ZeroEvent::EntityInit(account, block_number)),
-                topics: vec![],
-            }]
-        );
+		assert_eq!(
+			System::events(),
+			vec![EventRecord {
+				phase: Phase::Initialization,
+				event: Event::Sense(SenseEvent::EntityInit(account, block_number)),
+				topics: vec![],
+			}]
+		);
 
-        // TODO: Check Nonce value increased in storage as a result of successful extrinsic call.
+		// TODO: Check Nonce value increased in storage as a result of successful extrinsic call.
 
-        assert_noop!(
-            ZeroSense::create_entity(Origin::signed(1), 1, vec![1u8]),
-            BadOrigin
-        );
+		assert_noop!(Sense::create_entity(Origin::signed(1), 1, vec![1u8]), BadOrigin);
 
-        assert_noop!(
-            ZeroSense::create_entity(RawOrigin::Root.into(), account, cid.clone()),
-            Error::<Test>::EntityExists
-        );
-    });
+		assert_noop!(
+			Sense::create_entity(RawOrigin::Root.into(), account, cid.clone()),
+			Error::<Test>::EntityExists
+		);
+	});
 }
 
 // TODO: 1. Test: StorageMap value updated after calling extrinsic (SenseXP etc.)
-// 2. Tese: event is generated (EntityMutateXP etc.)
+// 2. Test: event is generated (EntityMutateXP etc.)
 // 3. Add comments
 macro_rules! sense_mod_tests {
     ($($name:ident: $storage:tt, $extrinsic:path,)*) => {
@@ -88,7 +76,7 @@ macro_rules! sense_mod_tests {
 					Error::<Test>::EntityUnknown
 				);
 
-				Sense::<Test>::insert(
+				SenseEntity::<Test>::insert(
 					account, Entity::new(account, block_number, 0, vec![1,2,3])
 				);
 				$storage::<Test>::insert(
@@ -105,7 +93,7 @@ macro_rules! sense_mod_tests {
 }
 
 sense_mod_tests! {
-    sense_mod_xp: SenseXP, ZeroSense::mod_xp,
-    sense_mod_rep: SenseREP, ZeroSense::mod_rep,
-    sense_mod_trust: SenseTrust, ZeroSense::mod_trust,
+	sense_mod_xp: SenseXP, Sense::mod_xp,
+	sense_mod_rep: SenseREP, Sense::mod_rep,
+	sense_mod_trust: SenseTrust, Sense::mod_trust,
 }
