e5d35707cc1ddb2b903c56b6064dde4ff2bb77a5
Vasylenko Yevhen <ggbigkon@gmail.com>
2022-06-16 13:17:15 +0300

    Add benchmarks to Control pallet (#42)
    * Implemented benchmarks for control pallet

--- traits/src/lib.rs
+++ traits/src/lib.rs
@@ -13,19 +13,37 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
+use frame_support::dispatch::DispatchError;
+use sp_std::vec::Vec;
+
+
 pub trait ControlTrait<AccountId, Hash> {
 
-    fn org_controller_account(org_id: &Hash) -> AccountId;
-    fn org_treasury_account(org_id: &Hash) -> AccountId;
-    fn is_org_active(org_id: &Hash) -> bool;
-    fn is_org_member_active(org_id: &Hash, accont_id: &AccountId) -> bool;
+	fn org_controller_account(org_id: &Hash) -> AccountId;
+	fn org_treasury_account(org_id: &Hash) -> AccountId;
+	fn is_org_active(org_id: &Hash) -> bool;
+	fn is_org_member_active(org_id: &Hash, accont_id: &AccountId) -> bool;
+}
+
+pub trait ControlBenchmarkingTrait<AccountId, Hash> {
+
+	/// Helper method to create organization.
+	/// ** Should be used for benchmarking only!!! **
+	#[cfg(feature = "runtime-benchmarks")]
+	fn create_org(caller: AccountId) -> Result<Hash, DispatchError>;
+
+	/// Helper method to add accounts to organisation.
+	/// It is assumed those accounts have enough of currency to pay org joining fee.
+	/// ** Should be used for benchmarking only!!! **
+	#[cfg(feature = "runtime-benchmarks")]
+	fn fill_org_with_members(org_id: &Hash, members: &Vec<AccountId>) -> Result<(), DispatchError>;
 }
 
 pub trait FlowTrait<AccountId, Balance, Hash> {
 
-    fn campaign_balance(campaign_id: &Hash) -> Balance;
-    fn is_campaign_succeeded(campaign_id: &Hash) -> bool;
-    fn campaign_contributors_count(campaign_id: &Hash) -> u64;
-    fn campaign_org(campaign_id: &Hash) -> Hash;
-    fn campaign_owner(campaign_id: &Hash) -> Option<AccountId>;
+	fn campaign_balance(campaign_id: &Hash) -> Balance;
+	fn is_campaign_succeeded(campaign_id: &Hash) -> bool;
+	fn campaign_contributors_count(campaign_id: &Hash) -> u64;
+	fn campaign_org(campaign_id: &Hash) -> Hash;
+	fn campaign_owner(campaign_id: &Hash) -> Option<AccountId>;
 }
