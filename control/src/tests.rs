d8319867d8e2894068310d324d2303151fb86548
Volodymyr Brazhnyk <crypto@vovacha.com>
2022-06-20 14:04:05 +0200

    Add control pallet tests, refactoring

--- control/src/tests.rs
+++ control/src/tests.rs
@@ -1,70 +1,155 @@
 #![cfg(test)]
 
 use frame_support::{assert_noop, assert_ok};
+use sp_runtime::traits::BadOrigin;
+use sp_core::H256;
 use super::*;
 use mock::*;
 
-#[test]
-fn control_create_campaign_success() {
-	new_test_ext().execute_with(|| {
-		System::set_block_number(3);
-		// create a DAO with account #5.
-		assert_ok!(Control::create_org(
-			Origin::signed(ALICE),
-			BOB,
-			vec![12, 56],
-			vec![11, 111],
-			Default::default(),
-			Default::default(),
-			Default::default(),
-			1 * DOLLARS,
-			0,
-			0,
-			10,
-            Some(1 * DOLLARS)
-		));
 
-		// check that there are now 1 Control in storage
-		assert_eq!(Nonce::<Test>::get(), 1);
-		let created_org_id = OrgByNonce::<Test>::get(0).unwrap();
-		let treasury = OrgTreasury::<Test>::get(created_org_id);
+fn create_org() -> H256 {
+	let nonce: u128 = Nonce::<Test>::get();
+	assert_ok!(
+		Control::create_org(Origin::signed(ALICE), BOB, vec![1, 2], vec![2, 3],
+		Default::default(), Default::default(), Default::default(),
+		1 * DOLLARS, PROTOCOL_TOKEN_ID, PAYMENT_TOKEN_ID, 10, None
+	));
+	OrgByNonce::<Test>::get(nonce).unwrap()
+}
+
+
+#[test]
+fn control_create_org() {
+	new_test_ext().execute_with(|| {
+		let current_block = 3;
+		System::set_block_number(current_block);
+		let org_id = create_org();
 		System::assert_has_event(
 			mock::Event::Control(
 				crate::Event::OrgCreated {
 					sender_id: ALICE,
-					org_id: created_org_id,
-					treasury_id: treasury,
-					created_at: System::block_number(),
+					org_id,
+					treasury_id: OrgTreasury::<Test>::get(org_id),
+					created_at: current_block,
 					realm_index: 0
 				}
 			)
 		);
+		// Check if creator (sender) has enough protocol token free balance 
+		// to make a deposit into org's treasury
+		// Error: BalanceTooLow
+		assert_noop!(
+			Control::create_org(Origin::signed(BOB), BOB, vec![1, 2], vec![2, 3],
+			Default::default(), Default::default(), Default::default(),
+			1 * DOLLARS, PROTOCOL_TOKEN_ID, PAYMENT_TOKEN_ID, 10, None),
+			
+			Error::<Test>::BalanceTooLow
+		);
+		// Check if creator (sender) has enough protocol token free balance 
+		// to make a deposit into org's treasury (custom deposit value)
+		// Error: BalanceTooLow
+		let deposit = 101 * DOLLARS;
+		assert_noop!(
+			Control::create_org(Origin::signed(BOB), BOB, vec![1, 2], vec![2, 3],
+			Default::default(), Default::default(), Default::default(),
+			1 * DOLLARS, PROTOCOL_TOKEN_ID, PAYMENT_TOKEN_ID, 10, Some(deposit)),
+			
+			Error::<Test>::BalanceTooLow
+		);
+	})
+}
 
-		// // check that account #5 is creator
-		// let creator_hash = <OrgByHash<Test>>::get(0);
-		// assert_eq!(Control::body_creator(creator_hash), 5);
+#[test]
+fn control_enable_deisable_org() {
+	new_test_ext().execute_with(|| {
+		let current_block = 3;
+		System::set_block_number(current_block);
+		let org_id = create_org();
+		// Disable org
+		assert_noop!(Control::disable_org(Origin::signed(BOB), org_id), BadOrigin);
+		assert_ok!(Control::disable_org(Origin::root(), org_id));
+		assert_eq!(OrgState::<Test>::get(org_id), ControlState::Inactive);
+		System::assert_has_event(
+			mock::Event::Control(crate::Event::OrgDisabled(org_id))
+		);
+		// Enable org
+		assert_noop!(Control::enable_org(Origin::signed(BOB), org_id), BadOrigin);
+		assert_ok!(Control::enable_org(Origin::root(), org_id));
+		assert_eq!(OrgState::<Test>::get(org_id), ControlState::Active);
+		System::assert_has_event(
+			mock::Event::Control(crate::Event::OrgEnabled(org_id))
+		);
 
-		// // check that account #4 is controller
-		// let controller_hash = <OrgByHash<Test>>::get(0);
-		// assert_eq!(Control::body_controller(controller_hash), 4);
+	})
+}
 
-		// // check that account #3 is treasury
-		// let treasury_hash = <OrgByHash<Test>>::get(0);
-		// assert_eq!(Control::body_treasury(treasury_hash), 3);
+#[test]
+fn control_add_remove_member() {
+	new_test_ext().execute_with(|| {
+		let current_block = 3;
+		System::set_block_number(current_block);
+		let org_id = create_org();
+		// Add member
+		assert_ok!(Control::add_member(Origin::signed(BOB), org_id, CHARLIE));
+		assert!(OrgMembers::<Test>::get(org_id).contains(&CHARLIE));
+		System::assert_has_event(
+			mock::Event::Control(crate::Event::AddMember{
+				org_id, account_id: CHARLIE, added_at: current_block
+			})
+		);
+		// Remove member
+		assert_ok!(Control::remove_member(Origin::signed(BOB), org_id, CHARLIE));
+		assert!(!OrgMembers::<Test>::get(org_id).contains(&CHARLIE));
+		System::assert_has_event(
+			mock::Event::Control(crate::Event::RemoveMember{
+				org_id, account_id: CHARLIE, removed_at: current_block
+			})
+		);
+	})
+}
 
-		// //
+#[test]
+fn control_add_remove_member_errors() {
+	new_test_ext().execute_with(|| {
+		let current_block = 3;
+		System::set_block_number(current_block);
+		let org_id = create_org();
+		
+		// Add member
 
-		// // check that account #6 owns 1 Campaign
-		// assert_eq!(Control::campaigns_owned_count(5), 1);
+		// Check if org already exists
+		// Error: OrganizationUnknown
+		assert_noop!(
+			Control::add_member(Origin::signed(BOB), H256::random(), CHARLIE),
+			Error::<Test>::OrganizationUnknown
+		);
+		// Check if member already exists
+		// Error: MemberExists
+		assert_noop!(
+			Control::add_member(Origin::signed(BOB), org_id, BOB),
+			Error::<Test>::MemberExists
+		);
+		// Check if member limit does not reached
+		// Error: MembershipLimitReached
+		assert_ok!(Control::add_member(Origin::signed(BOB), org_id, CHARLIE));
+		assert_noop!(
+			Control::add_member(Origin::signed(BOB), org_id, DAVE),
+			Error::<Test>::MembershipLimitReached
+		);
 
-		// // check that some random account #3 does not own a Campaign
-		// assert_eq!(Control::campaigns_owned_count(3), 0);
+		// Remove member
 
-		// // check that this Campaign is specifically owned by account #6
-		// let owner_hash = Control::campaign_by_id(0);
-		// assert_eq!(Control::campaign_owner(owner_hash), Some(5));
-
-		// // let other_hash = Control::campaigns_owned_index(5, 0);
-		// // assert_eq!(hash, other_hash);
+		// Check if org already exists
+		// Error: OrganizationUnknown
+		assert_noop!(
+			Control::remove_member(Origin::signed(BOB), H256::random(), CHARLIE),
+			Error::<Test>::OrganizationUnknown
+		);
+		// Check if member exists
+		// Error: MemberUnknown
+		assert_noop!(
+			Control::remove_member(Origin::signed(BOB), org_id, DAVE),
+			Error::<Test>::MemberUnknown
+		);
 	})
 }
