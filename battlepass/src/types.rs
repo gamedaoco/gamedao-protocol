4e017b656442be778edeb84027dace06465a3407
FiberMan <filimonchuk@gmail.com>
2022-12-15 12:44:38 +0100

    Create Battlepass pallete

--- battlepass/src/types.rs
+++ battlepass/src/types.rs
@@ -3,32 +3,35 @@ use codec::MaxEncodedLen;
 
 #[derive(Encode, Decode, Clone, PartialEq, Eq, PartialOrd, Ord, TypeInfo, MaxEncodedLen)]
 pub enum BattlepassState {
-	Draft = 0,
-	Active = 1,
-	Closed = 2,
+	DRAFT = 0,
+	ACTIVE = 1,
+	CLOSED = 2,
 }
 impl Default for BattlepassState {
 	fn default() -> Self {
-		Self::Draft
+		Self::DRAFT
 	}
 }
 
 #[derive(Encode, Decode, Default, PartialEq, Eq, TypeInfo, MaxEncodedLen)]
-pub struct Battlepass<Hash, AccountId, BlockNumber, BoundedString> {
+/// Battlepass struct
+///
+/// `collection_id`: Collection that will store all claimed Battlepass-NFTs
+pub struct Battlepass<Hash, AccountId, BoundedString> {
 	pub creator: AccountId,
 	pub org_id: Hash,
 	pub name: BoundedString,
 	pub cid: BoundedString,
-    pub season: u32,
-    pub price: u16,				// TODO: introduce currency
+	pub season: u32,
+	pub price: u16,				// TODO: introduce currency
 	/// Collection that will store all claimed Battlepass-NFTs
-	pub collection_id: u32,
-	pub created: BlockNumber,
-	pub mutated: BlockNumber,
+	pub collection_id: u32
 }
 
 #[derive(Encode, Decode, Default, PartialEq, Eq, TypeInfo, MaxEncodedLen)]
-pub struct BattlepassNft {
-	pub collection_id: u32,
-	pub nft_id: u32,
+pub struct BattlepassInfo<Hash> {
+	/// Total number of battlepasses per organization.
+	pub count: u32,
+	/// Curent active battlepass
+	pub active: Option<Hash>
 }
