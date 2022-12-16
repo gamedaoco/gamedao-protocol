120377292436307aeade7efd63ba03088981efb6
FiberMan <filimonchuk@gmail.com>
2022-12-16 11:02:59 +0100

    Update extrinsics.

--- battlepass/src/lib.rs
+++ battlepass/src/lib.rs
@@ -17,6 +17,10 @@ use frame_support::pallet_prelude::*;
 use frame_system::pallet_prelude::*;
 use sp_std::convert::TryInto;
 use sp_runtime::traits::Hash;
+use gamedao_traits::ControlTrait;
+// use pallet_rmrk_core::*;
+// use rmrk_traits::{primitives::*, Collection, Nft};
+// use rmrk_traits::{primitives::*, Collection};
 
 pub mod types;
 pub use types::*;
@@ -37,9 +41,17 @@ pub mod pallet {
 			+ IsType<<Self as frame_system::Config>::Event>
 			+ Into<<Self as frame_system::Config>::Event>;
 
+		type Control: ControlTrait<Self::AccountId, Self::Hash>;
+
+		// type Rmrk: rmrk_traits::Collection<String<Self>, Self::BoundedSymbol, Self::AccountId>;
+
 		/// The maximum length of a name or cid stored on-chain.
 		#[pallet::constant]
 		type StringLimit: Get<u32>;
+
+		/// The maximum number of battlepasses in organization.
+		#[pallet::constant]
+		type MaxBattlepassesPerOrg: Get<u32>;
 	}
 
 	#[pallet::event]
@@ -47,9 +59,9 @@ pub mod pallet {
 	pub enum Event<T: Config> {
 		/// New BattlePass created
 		BattlepassCreated {
-			// org_id: T::Hash,
 			org_id: T::Hash,
 			battlepass_id: T::Hash,
+			season: u32,
 			block_number: T::BlockNumber
 		},
 		
@@ -61,6 +73,13 @@ pub mod pallet {
 			nft_id: T::Hash,
 			block_number: T::BlockNumber
 		},
+
+		/// BattlePass closed
+		BattlepassClosed {
+			closed_by: T::AccountId,
+			org_id: T::Hash,
+			battlepass_id: T::Hash
+		},
 	}
 
 	#[pallet::error]
@@ -68,11 +87,13 @@ pub mod pallet {
 		AuthorizationError,
 		MissingParameter,
 		InvalidParameter,
-		OrganizationUnknown,
 		OrgHasActiveBattlepass,
+		OrgPrimeUnknown,
+		OrgUnknown,
 		BattlepassExists,
 		BattlepassClaimed,
 		BattlepassUnknown,
+		BattlepassLimitReached,
 		NotMember,
 	  }
 
@@ -83,7 +104,14 @@ pub mod pallet {
 	#[pallet::getter(fn get_battlepass)]
 	pub(super) type Battlepasses<T: Config> = StorageMap<_, Blake2_128Concat, T::Hash, Battlepass<T::Hash, T::AccountId, T::BlockNumber, String<T>>, OptionQuery>;
 
-	/// Number of battlepasses per organization.
+	/// Battlepass state.
+	///
+	/// BattlepassStates: map Hash => BattlepassState
+	#[pallet::storage]
+	#[pallet::getter(fn get_battlepass_state)]
+	pub type BattlepassStates<T: Config> = StorageMap<_, Blake2_128Concat, T::Hash, BattlepassState, OptionQuery>;
+
+	/// Total number of battlepasses per organization.
 	///
 	/// BattlepassCount: map Hash => u32
 	#[pallet::storage]
@@ -120,32 +148,38 @@ pub mod pallet {
 			price: u16,
 		) -> DispatchResult {
 			let creator = ensure_signed(origin)?;
-
-			// check if origin is Org Prime or Root
-
 			// check if Org exists
-
-			// check if active battlepass does not exist for the Org
-
-
-			
-			// Create Battlepass
+			ensure!(T::Control::is_org_active(&org_id), Error::<T>::OrgUnknown);
+			// check if origin is an Org Prime
+			let prime = T::Control::org_prime_account(&org_id).ok_or(Error::<T>::OrgPrimeUnknown)?;
+			ensure!(creator == prime, Error::<T>::AuthorizationError);  // TODO: Add Root temporaryly
+			// check is there is no active battlepass for the Org
+			ensure!(!ActiveBattlepassByOrg::<T>::contains_key(&org_id), Error::<T>::BattlepassExists);
+			let new_season = Self::get_battlepass_count(org_id) + 1;
+			// check if battlepass count reached the limit for the Org
+			ensure!(new_season <= T::MaxBattlepassesPerOrg::get(), Error::<T>::BattlepassLimitReached);
 			let now = <frame_system::Pallet<T>>::block_number();
 			let battlepass: Battlepass<T::Hash, T::AccountId, T::BlockNumber, String<T>> = types::Battlepass {
 				creator, 
 				org_id,
 				name,
 				cid,
-				state: types::BattlepassState::Active,
-				season: Self::get_battlepass_count(org_id),
+				season: new_season,
 				price,
 				created: now.clone(), mutated: now
 			};
 			let battlepass_id = <T as frame_system::Config>::Hashing::hash_of(&battlepass);
 
 			Battlepasses::<T>::insert(&battlepass_id, battlepass);
+			BattlepassStates::<T>::insert(&battlepass_id, types::BattlepassState::Active); // TODO: change to Draft
+			ActiveBattlepassByOrg::<T>::insert(org_id, battlepass_id);
+			BattlepassCount::<T>::insert(org_id, new_season);
 
-			Self::deposit_event(Event::BattlepassCreated { org_id, battlepass_id, block_number: now });
+			// <pallet_rmrk_core::Pallet<T>>::nfts(1, 2);
+			// pallet_rmrk_core::Call::mint_nft { owner: (), nft_id: (), collection_id: (), royalty_recipient: (), royalty: (), metadata: (), transferable: (), resources: () };
+			// let col = pallet_rmrk_core::Collections::<T>::create_collection(origin, 0x87241, 10, 0x9873);
+
+			Self::deposit_event(Event::BattlepassCreated { org_id, battlepass_id, season: new_season, block_number: now });
 
 			Ok(())
 		}
@@ -156,19 +190,15 @@ pub mod pallet {
 			org_id: T::Hash,
 		) -> DispatchResult {
 			let claimer = ensure_signed(origin)?;
+			// check if Org exists
+			ensure!(T::Control::is_org_active(&org_id), Error::<T>::OrgUnknown);
+			// check if user is a member of organization
+			ensure!(T::Control::is_org_member_active(&org_id, &claimer), Error::<T>::NotMember);
+			// check if Org has active battlepass
 			let battlepass_id = Self::get_active_battlepass(org_id).ok_or(Error::<T>::BattlepassUnknown)?;
 			let now = <frame_system::Pallet<T>>::block_number();
-
-			// check if user is a member of organization
-
-			// check if Org exists
-
-			// check if Org has active battlepass
-
 			// check if Battlepass already claimed
-
-			
-			
+			ensure!(!ClaimedBattlepass::<T>::contains_key(claimer.clone(), battlepass_id), Error::<T>::BattlepassClaimed);
 
 			// Create NFT
 			let nft_id = <T as frame_system::Config>::Hashing::hash_of(&claimer);
@@ -177,6 +207,28 @@ pub mod pallet {
 			ClaimedBattlepass::<T>::insert(&claimer, battlepass_id, nft_id);
 
 			Self::deposit_event(Event::BattlepassClaimed { claimer, org_id, battlepass_id, nft_id, block_number: now });
+
+			Ok(())
+		}
+
+		#[pallet::weight(0)]
+		pub fn close_battlepass(
+			origin: OriginFor<T>,
+			org_id: T::Hash,
+		) -> DispatchResult {
+			let sender = ensure_signed(origin)?;
+			// check if Org exists
+			ensure!(T::Control::is_org_active(&org_id), Error::<T>::OrgUnknown);
+			// check if origin is an Org Prime
+			let prime = T::Control::org_prime_account(&org_id).ok_or(Error::<T>::OrgPrimeUnknown)?;
+			ensure!(sender == prime, Error::<T>::AuthorizationError);  // TODO: Add Root temporaryly
+			// check if Org has active battlepass
+			let battlepass_id = Self::get_active_battlepass(org_id).ok_or(Error::<T>::BattlepassUnknown)?;
+
+			BattlepassStates::<T>::insert(&battlepass_id, types::BattlepassState::Closed); 
+			ActiveBattlepassByOrg::<T>::remove(org_id);
+
+			Self::deposit_event(Event::BattlepassClosed { closed_by: sender, org_id, battlepass_id });
 
 			Ok(())
 		}
--- battlepass/src/types.rs
+++ battlepass/src/types.rs
@@ -2,28 +2,26 @@ use frame_support::pallet_prelude::*;
 use codec::MaxEncodedLen;
 
 #[derive(Encode, Decode, Clone, PartialEq, Eq, PartialOrd, Ord, TypeInfo, MaxEncodedLen)]
-// #[cfg_attr(feature = "std", derive(Serialize, Deserialize, Debug))]
 pub enum BattlepassState {
-	Inactive = 0,
+	Draft = 0,
 	Active = 1,
+	Closed = 2,
 }
 impl Default for BattlepassState {
 	fn default() -> Self {
-		Self::Active
+		Self::Draft
 	}
 }
 
 
 #[derive(Encode, Decode, Default, PartialEq, Eq, TypeInfo, MaxEncodedLen)]
-// #[cfg_attr(feature = "std", derive(Serialize, Deserialize, Debug))]
 pub struct Battlepass<Hash, AccountId, BlockNumber, BoundedString> {
 	pub creator: AccountId,
 	pub org_id: Hash,
 	pub name: BoundedString,
 	pub cid: BoundedString,
-    pub state: BattlepassState,
     pub season: u32,
-    pub price: u16,
+    pub price: u16,				// TODO: introduce currency
 	pub created: BlockNumber,
 	pub mutated: BlockNumber,
 }
